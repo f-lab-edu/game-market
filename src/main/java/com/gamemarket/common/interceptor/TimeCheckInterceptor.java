93f9f7bcd5bfc65166d5523b34e4c73631a33cd8
Greg.Study <greg0917@naver.com>
2024-05-16 03:29:57 +0900

    refactor: interceptor 전역변수 제거

--- src/main/java/com/gamemarket/common/interceptor/TimeCheckInterceptor.java
+++ src/main/java/com/gamemarket/common/interceptor/TimeCheckInterceptor.java
@@ -13,25 +13,30 @@ import org.springframework.web.servlet.ModelAndView;
 public class TimeCheckInterceptor implements HandlerInterceptor {
 
     private static final long LATENCY = 2000;
-
-    private long start;
+    private static final String REQUEST_START_TIME = "start";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        start = System.currentTimeMillis();
+        request.setAttribute(REQUEST_START_TIME, System.currentTimeMillis());
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
-        long end = System.currentTimeMillis();
-        long processTime = end - start;
+        long processTime = getProcessTime(request);
 
         if (processTime > LATENCY) {
             latencyProcess(request, response, processTime);
         } else {
             normalProcess(request, response, processTime);
         }
+    }
+
+    private long getProcessTime(HttpServletRequest request) {
+        long requestEndTime = System.currentTimeMillis();
+        long requestStartTime = (long) request.getAttribute(REQUEST_START_TIME);
+
+        return requestEndTime - requestStartTime;
     }
 
     private void latencyProcess(HttpServletRequest request, HttpServletResponse response, long processTime) {
